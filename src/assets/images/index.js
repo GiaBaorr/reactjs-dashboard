540e76d1df2c2a845dda8510d1e35931face00a1
Tran Gia_Bao <baozach123@gmail.com>
2022-10-30 22:53:54 +0700

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+const images = {
+  bgHeroPattern: require('./bg-hero-pattern.svg').default,
+};
+
+export default images;
---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const inititalState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(inititalState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const handleClick = (clicked) => {
+    setIsClicked({ ...inititalState, [clicked]: true });
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
